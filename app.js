804e75eec03af2a03c786697c872f143f9f221b8
Mubin Wang <mubin.w@gmail.com>
2016-01-18 20:33:15 -0800

    done basic work

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+
+// create a variable 'project' with existing controller
+// use require();
+var project = require('./routes/project');
+
 // Example route
 // var user = require('./routes/user');
 
@@ -36,6 +41,7 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/project/:name', project.viewProject); // bind url & controller action
 // Example route
 // app.get('/users', user.list);
 
---
+++ routes/project.js
@@ -0,0 +1,13 @@
+/**
+ * Created by MuyBien on 1/18/16.
+ */
+exports.viewProject = function(req, res) {
+    // controller code goes here 
+
+    var name = req.params.name;
+    console.log("The project name is: " + name);
+
+    res.render('project', {
+        'projectName' : name // assign value to a handlebar expression
+    });
+};
